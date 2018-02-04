22165fe7bbc22ad73cfc51164837ea40ac923a1a
Nethanel Kohen <nethanel.kohen@gmail.com>
2018-02-04 15:32:43 -0500

    notes: Ingredients.jsx

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// Stateless functional component that maps through data from Edamam API call
+// and returns ingredients objects.
 const Ingredients = ({ ingredients }) => (
   <div>
-    <ul className='ingredients'>
-      {ingredients.map(function(name, index){
-          return <li key={index}>{name}</li>;
-          })}
+    <ul className="ingredients">
+      {ingredients.map(function(name, index) {
+        return <li key={index}>{name}</li>;
+      })}
     </ul>
-   </div>
+  </div>
 );
 
+// Type check Ingredients to make sure type is array.
 Ingredients.propTypes = {
   ingredients: PropTypes.array.isRequired
 };
